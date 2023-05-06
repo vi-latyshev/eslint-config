f6fe64806450d50f4fee1f1d38acfb8d07e1a4df
vi.latyshev <storis2012@yandex.ru>
2023-05-06 16:58:39 +0300

    feat: first draft of eslint config

---
+++ index.js
@@ -0,0 +1,185 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { rules: baseStyleRules } = require('eslint-config-airbnb-base/rules/style');
+
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+    root: true,
+    extends: [
+        'airbnb-base',
+    ],
+    env: {
+        es6: true,
+        node: true,
+        browser: true,
+    },
+    parserOptions: {
+        sourceType: 'module',
+    },
+    rules: {
+        // http://eslint.org/docs/rules/
+        indent: [
+            'error',
+            4,
+            {
+                SwitchCase: 1,
+            },
+        ],
+        'no-underscore-dangle': [
+            'error',
+            {
+                enforceInClassFields: false,
+                allowAfterThis: true,
+            },
+        ],
+        'max-len': [
+            'error',
+            {
+                code: 120,
+                ignoreStrings: true,
+                ignoreRegExpLiterals: true,
+                ignoreTemplateLiterals: true,
+            },
+        ],
+        'linebreak-style': ['error', 'unix'],
+        'func-style': ['error', 'expression'],
+        // @TODO - add more statements
+        'no-restricted-syntax': [
+            'error',
+            'LabeledStatement',
+            'WithStatement',
+            "BinaryExpression[operator='in']",
+            'IfStatement > :not(BlockStatement).consequent',
+        ],
+        radix: [
+            'error',
+            'as-needed',
+        ],
+        'newline-before-return': 'error',
+
+        // https://github.com/benmosher/eslint-plugin-import/tree/master/docs/rules
+        'import/no-default-export': 'error',
+        'import/prefer-default-export': 'off',
+        'import/order': [
+            'error',
+            {
+                groups: [
+                    [
+                        'builtin',
+                        'external',
+                    ],
+                    'internal',
+                    'parent',
+                    'sibling',
+                    'type',
+                ],
+                pathGroups: [
+                    {
+                        pattern: '*(types)/**',
+                        group: 'internal',
+                        position: 'after',
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ['builtin', 'type'],
+                'newlines-between': 'always-and-inside-groups',
+            },
+        ],
+
+    },
+    overrides: [
+        {
+            files: ['**/*.ts?(x)'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                ecmaVersion: 2018,
+                sourceType: 'module',
+                project: './tsconfig.eslint.json',
+                tsconfigRootDir: __dirname,
+
+                // typescript-eslint specific options
+                warnOnUnsupportedTypeScriptVersion: true,
+            },
+            extends: [
+                'airbnb-typescript/base',
+            ],
+            // If adding a typescript-eslint version of
+            // an existing ESLint (equivalents) rule,
+            // make sure to disable the ESLint rule here.
+            rules: {
+                'no-shadow': 'off',
+
+                // https://typescript-eslint.io/rules/
+                '@typescript-eslint/no-shadow': 'error',
+                '@typescript-eslint/no-explicit-any': 'error',
+
+                semi: 'off',
+                '@typescript-eslint/semi': ['error', 'always'],
+                quotes: 'off',
+                '@typescript-eslint/quotes': 'warn',
+                indent: 'off',
+                '@typescript-eslint/indent': [
+                    'error',
+                    4,
+                    {
+                        ...baseStyleRules.indent[2],
+                        SwitchCase: 1,
+                        // issues https://github.com/typescript-eslint/typescript-eslint/issues/1824
+                        ignoredNodes: [
+                            ...baseStyleRules.indent[2].ignoredNodes,
+                            'PropertyDefinition[decorators]', // ignore decorators
+                            'TSUnionType', // ignore union of types
+                            'FunctionExpression[params]:has(Identifier[decorators])',
+                        ],
+                    },
+                ],
+                'no-unused-vars': 'off',
+                '@typescript-eslint/no-unused-vars': [
+                    'warn',
+                    {
+                        argsIgnorePattern: '^_',
+                    },
+                ],
+                'comma-dangle': 'off',
+                '@typescript-eslint/comma-dangle': [
+                    'error',
+                    { // dont remove
+                        arrays: 'always-multiline',
+                        objects: 'always-multiline',
+                        imports: 'always-multiline',
+                        exports: 'always-multiline',
+                        functions: 'only-multiline',
+                        enums: 'always-multiline',
+                        generics: 'never',
+                        tuples: 'always-multiline',
+                    },
+                ],
+                '@typescript-eslint/type-annotation-spacing': 'error',
+                '@typescript-eslint/explicit-module-boundary-types': 'error',
+                '@typescript-eslint/lines-between-class-members': ['error', 'never'],
+                '@typescript-eslint/member-ordering': [
+                    'error',
+                    {
+                        default: [
+                            'public-field',
+                            'protected-field',
+                            'private-field',
+
+                            'constructor',
+
+                            'public-abstract-field',
+                            'protected-abstract-field',
+
+                            'public-abstract-method',
+                            'protected-abstract-method',
+
+                            'public-method',
+                            'protected-method',
+                            'private-method',
+                        ],
+                    },
+                ],
+            },
+        },
+    ],
+};
---
+++ react.js
@@ -0,0 +1,47 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+    extends: [
+        require.resolve('.'),
+    ],
+    overrides: [
+        {
+            files: ['**/*.ts?(x)'],
+            // If adding a typescript-eslint version of
+            // an existing ESLint (equivalents) rule,
+            // make sure to disable the ESLint rule here.
+            rules: {
+                // react/
+                'react/jsx-indent': [
+                    'error',
+                    4,
+                ],
+                'react/jsx-indent-props': [
+                    'error',
+                    4,
+                ],
+                'react/jsx-props-no-spreading': 'off',
+                'react/jsx-max-props-per-line': ['error', {
+                    maximum: {
+                        single: 3,
+                        multi: 1,
+                    },
+                }],
+                'react/prop-types': 'off',
+                'react/jsx-one-expression-per-line': 'off',
+                'react/no-unescaped-entities': 'warn',
+                'react/react-in-jsx-scope': 'off',
+                'react/require-default-props': 'off',
+                'react/display-name': 'off',
+                'react/function-component-definition': ['error', {
+                    namedComponents: 'arrow-function',
+                }],
+                'react-hooks/exhaustive-deps': 'off',
+
+                // jsx-a11y
+                'jsx-a11y/anchor-is-valid': 'off',
+            },
+        },
+    ],
+};
